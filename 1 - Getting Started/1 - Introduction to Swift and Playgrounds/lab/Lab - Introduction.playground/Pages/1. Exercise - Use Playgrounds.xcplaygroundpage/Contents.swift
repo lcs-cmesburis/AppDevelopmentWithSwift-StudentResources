b06436c33d1e741b25e83b17e163840dd797b4dc
Mesburis, Cassie <cmesburis@lcs-mba3252.lcs.on.ca>
2017-12-04 14:36:32 -0500

    1.1 complete

--- 1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -3,19 +3,16 @@
  
  The code below prints a few short statements about what you have learned in this lesson. Open the console area and view the code's output.
  */
-print("I have learned the following:")
-print("What features make Swift a modern and safe language")
-print("How to use the Swift REPL in Terminal")
-print("How to use playgrounds to make writing Swift fun and simple")
+
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("All I want for Christmas by Mariah Carey.")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
+print("I don't want a lot for Christmas there is just one thing I need...")
 
 
 /*:
