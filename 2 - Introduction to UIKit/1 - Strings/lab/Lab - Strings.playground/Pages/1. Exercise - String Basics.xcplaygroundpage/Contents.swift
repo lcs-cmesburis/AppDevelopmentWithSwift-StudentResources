efff1a1eb426d6488f1e013a1ac2eab999d19232
Mesburis, Cassie <cmesburis@lcs-mba3252.lcs.on.ca>
2017-12-14 09:50:52 -0500

    complete 1.8

--- 2 - Introduction to UIKit/1 - Strings/lab/Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/1 - Strings/lab/Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
@@ -3,7 +3,7 @@
  
  Create a `name` constant and assign it a string literal representing your name.
  */
-
+let cassie = String(Cassie)
 
 /*:
  Create a `favoriteQuote` constant and assign it the following string literal:
@@ -12,8 +12,8 @@
  
  Write in your own favorite quote where indicated, and be sure to include escaped quotation marks. Print the value of `favoriteQuote`
  */
-
-
+let favoriteQuote = String("My favorite quote is <The difference between stupidity and genius is that genius has its limits.>")
+print(""My favorite quote is <The difference between stupidity and genius is that genius has its limits.>"")
 /*:
  Write an if-else statement that prints "There's nothing here" if `emptyString` is empty, and "It's not as empty as I thought" otherwise.
  */
