12ede325ac8d4779ea0d2851a705efe89fb569d8
Mesburis, Cassie <cmesburis@lcs-mba3252.lcs.on.ca>
2017-12-14 10:25:05 -0500

    complete pg 9

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/9. Exercise - Type Inference and Required Values.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/9. Exercise - Type Inference and Required Values.xcplaygroundpage/Contents.swift
@@ -1,8 +1,3 @@
-/*:
- ## Exercise - Type Inference and Required Values
- 
- Declare a variable called `name` of type `String`, but do not give it a value. Print `name` to the console. Does the code compile? Remove any code that will not compile.
- */
 
 
 /*:
@@ -13,11 +8,12 @@
 /*:
  Declare a variable called `distanceTraveled` and set it to 0. Do not give it an explicit type.
  */
-
+var distanceTraveled = 0.00
 
 /*:
  Now assign a value of 54.3 to `distanceTraveled`. Does the code compile? Go back and set an explicit type on `distanceTraveled` so the code will compile.
  */
+distanceTraveled = 54.3
 
 
 //: [Previous](@previous)  |  page 9 of 10  |  [Next: App Exercise - Percent Completed](@next)
